326c23c4a298167a4815b072e99647401b2428c3
Paul Hachmang <paul@h-o.nl>
2016-06-22 14:21:20 +0200

    PROMO-39: Workaround for core bug, tierprices not imported correctly

---
+++ Rewrite/ImportExport/Import/Product.php
@@ -0,0 +1,350 @@
+<?php
+/**
+ * Copyright (c) 2016 H&O E-commerce specialisten B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ho\Import\Rewrite\ImportExport\Import;
+
+use Magento\CatalogImportExport\Model\Import\Product\RowValidatorInterface as ValidatorInterface;
+use Magento\Framework\Stdlib\DateTime;
+use Magento\ImportExport\Model\Import;
+use Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingError;
+
+class Product extends \Magento\CatalogImportExport\Model\Import\Product
+{
+
+    /**
+     * Product entity link field
+     *
+     * @var string
+     */
+    private $productEntityLinkField;
+
+    /**
+     * Product entity identifier field
+     *
+     * @var string
+     */
+    private $productEntityIdentifierField;
+
+    /**
+     * Gather and save information about product entities.
+     * Waiting for https://github.com/magento/magento2/issues/5160 to be resolved.
+     *
+     * @return $this
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    protected function _saveProducts()
+    {
+        $priceIsGlobal = $this->_catalogData->isPriceGlobal();
+        $productLimit = null;
+        $productsQty = null;
+
+        while ($bunch = $this->_dataSourceModel->getNextBunch()) {
+            $entityRowsIn = [];
+            $entityRowsUp = [];
+            $attributes = [];
+            $this->websitesCache = [];
+            $this->categoriesCache = [];
+            $tierPrices = [];
+            $mediaGallery = [];
+            $uploadedImages = [];
+            $previousType = null;
+            $prevAttributeSet = null;
+            $existingImages = $this->getExistingImages($bunch);
+
+            foreach ($bunch as $rowNum => $rowData) {
+                if (!$this->validateRow($rowData, $rowNum)) {
+                    continue;
+                }
+                if ($this->getErrorAggregator()->hasToBeTerminated()) {
+                    $this->getErrorAggregator()->addRowToSkip($rowNum);
+                    continue;
+                }
+                $rowScope = $this->getRowScope($rowData);
+
+                $rowSku = $rowData[self::COL_SKU];
+
+                if (null === $rowSku) {
+                    $this->getErrorAggregator()->addRowToSkip($rowNum);
+                    continue;
+                } elseif (self::SCOPE_STORE == $rowScope) {
+                    // set necessary data from SCOPE_DEFAULT row
+                    $rowData[self::COL_TYPE] = $this->skuProcessor->getNewSku($rowSku)['type_id'];
+                    $rowData['attribute_set_id'] = $this->skuProcessor->getNewSku($rowSku)['attr_set_id'];
+                    $rowData[self::COL_ATTR_SET] = $this->skuProcessor->getNewSku($rowSku)['attr_set_code'];
+                }
+
+                // 1. Entity phase
+                if (isset($this->_oldSku[$rowSku])) {
+                    // existing row
+                    $entityRowsUp[] = [
+                        'updated_at' => (new \DateTime())->format(DateTime::DATETIME_PHP_FORMAT),
+                        $this->getProductEntityLinkField()
+                            => $this->_oldSku[$rowSku][$this->getProductEntityLinkField()],
+                    ];
+                } else {
+                    if (!$productLimit || $productsQty < $productLimit) {
+                        $entityRowsIn[$rowSku] = [
+                            'attribute_set_id' => $this->skuProcessor->getNewSku($rowSku)['attr_set_id'],
+                            'type_id' => $this->skuProcessor->getNewSku($rowSku)['type_id'],
+                            'sku' => $rowSku,
+                            'has_options' => isset($rowData['has_options']) ? $rowData['has_options'] : 0,
+                            'created_at' => (new \DateTime())->format(DateTime::DATETIME_PHP_FORMAT),
+                            'updated_at' => (new \DateTime())->format(DateTime::DATETIME_PHP_FORMAT),
+                        ];
+                        $productsQty++;
+                    } else {
+                        $rowSku = null;
+                        // sign for child rows to be skipped
+                        $this->getErrorAggregator()->addRowToSkip($rowNum);
+                        continue;
+                    }
+                }
+
+                if (!array_key_exists($rowSku, $this->websitesCache)) {
+                    $this->websitesCache[$rowSku] = [];
+                }
+                // 2. Product-to-Website phase
+                if (!empty($rowData[self::COL_PRODUCT_WEBSITES])) {
+                    $websiteCodes = explode($this->getMultipleValueSeparator(), $rowData[self::COL_PRODUCT_WEBSITES]);
+                    foreach ($websiteCodes as $websiteCode) {
+                        $websiteId = $this->storeResolver->getWebsiteCodeToId($websiteCode);
+                        $this->websitesCache[$rowSku][$websiteId] = true;
+                    }
+                }
+
+                // 3. Categories phase
+                if (!array_key_exists($rowSku, $this->categoriesCache)) {
+                    $this->categoriesCache[$rowSku] = [];
+                }
+                $rowData['rowNum'] = $rowNum;
+                $categoryIds = $this->processRowCategories($rowData);
+                foreach ($categoryIds as $id) {
+                    $this->categoriesCache[$rowSku][$id] = true;
+                }
+                unset($rowData['rowNum']);
+
+                // 4.1. Tier prices phase
+                //_getMultiRowFormat
+
+
+                if (!empty($rowData['tier_prices'])) {
+                    $tierPriceData = json_decode($rowData['tier_prices'], true);
+
+                    foreach ($tierPriceData as $tierPrice) {
+                        $tierPrices[$rowSku][] = [
+                            'all_groups' => $tierPrice['website'] == self::VALUE_ALL,
+                            'customer_group_id' => $tierPrice['customer_group'] ==
+                            self::VALUE_ALL ? 0 : $tierPrice['customer_group'],
+                            'qty' => $tierPrice['qty'],
+                            'value' => $tierPrice['price'],
+                            'website_id' => self::VALUE_ALL == $tierPrice['website'] ||
+                            $priceIsGlobal ? 0 : $this->storeResolver->getWebsiteCodeToId($tierPrice['website']),
+                        ];
+                    }
+                }
+
+                if (!$this->validateRow($rowData, $rowNum)) {
+                    continue;
+                }
+
+                // 5. Media gallery phase
+                $disabledImages = [];
+                list($rowImages, $rowLabels) = $this->getImagesFromRow($rowData);
+                if (isset($rowData['_media_is_disabled'])) {
+                    $disabledImages = array_flip(
+                        explode($this->getMultipleValueSeparator(), $rowData['_media_is_disabled'])
+                    );
+                }
+                $rowData[self::COL_MEDIA_IMAGE] = [];
+                foreach ($rowImages as $column => $columnImages) {
+                    foreach ($columnImages as $position => $columnImage) {
+                        if (!isset($uploadedImages[$columnImage])) {
+                            $uploadedFile = $this->uploadMediaFiles(trim($columnImage), true);
+                            if ($uploadedFile) {
+                                $uploadedImages[$columnImage] = $uploadedFile;
+                            } else {
+                                $this->addRowError(
+                                    ValidatorInterface::ERROR_MEDIA_URL_NOT_ACCESSIBLE,
+                                    $rowNum,
+                                    null,
+                                    null,
+                                    ProcessingError::ERROR_LEVEL_NOT_CRITICAL
+                                );
+                            }
+                        } else {
+                            $uploadedFile = $uploadedImages[$columnImage];
+                        }
+
+                        if ($uploadedFile && $column !== self::COL_MEDIA_IMAGE) {
+                            $rowData[$column] = $uploadedFile;
+                        }
+
+                        $imageNotAssigned = !isset($existingImages[$rowSku][$uploadedFile]);
+
+                        if ($uploadedFile && $imageNotAssigned) {
+                            if ($column == self::COL_MEDIA_IMAGE) {
+                                $rowData[$column][] = $uploadedFile;
+                            }
+                            $mediaGallery[$rowSku][] = [
+                                'attribute_id' => $this->getMediaGalleryAttributeId(),
+                                'label' => isset($rowLabels[$column][$position]) ? $rowLabels[$column][$position] : '',
+                                'position' => $position + 1,
+                                'disabled' => isset($disabledImages[$columnImage]) ? '1' : '0',
+                                'value' => $uploadedFile,
+                            ];
+                            $existingImages[$rowSku][$uploadedFile] = true;
+                        }
+                    }
+                }
+
+                // 6. Attributes phase
+                $rowStore = (self::SCOPE_STORE == $rowScope)
+                    ? $this->storeResolver->getStoreCodeToId($rowData[self::COL_STORE])
+                    : 0;
+                $productType = isset($rowData[self::COL_TYPE]) ? $rowData[self::COL_TYPE] : null;
+                if (!is_null($productType)) {
+                    $previousType = $productType;
+                }
+                if (isset($rowData[self::COL_ATTR_SET])) {
+                    $prevAttributeSet = $rowData[self::COL_ATTR_SET];
+                }
+                if (self::SCOPE_NULL == $rowScope) {
+                    // for multiselect attributes only
+                    if (!is_null($prevAttributeSet)) {
+                        $rowData[self::COL_ATTR_SET] = $prevAttributeSet;
+                    }
+                    if (is_null($productType) && !is_null($previousType)) {
+                        $productType = $previousType;
+                    }
+                    if (is_null($productType)) {
+                        continue;
+                    }
+                }
+
+                $productTypeModel = $this->_productTypeModels[$productType];
+                if (!empty($rowData['tax_class_name'])) {
+                    $rowData['tax_class_id'] =
+                        $this->taxClassProcessor->upsertTaxClass($rowData['tax_class_name'], $productTypeModel);
+                }
+
+                if ($this->getBehavior() == Import::BEHAVIOR_APPEND ||
+                    empty($rowData[self::COL_SKU])
+                ) {
+                    $rowData = $productTypeModel->clearEmptyData($rowData);
+                }
+
+                $rowData = $productTypeModel->prepareAttributesWithDefaultValueForSave(
+                    $rowData,
+                    !isset($this->_oldSku[$rowSku])
+                );
+                $product = $this->_proxyProdFactory->create(['data' => $rowData]);
+
+                foreach ($rowData as $attrCode => $attrValue) {
+                    $attribute = $this->retrieveAttributeByCode($attrCode);
+
+                    if ('multiselect' != $attribute->getFrontendInput() && self::SCOPE_NULL == $rowScope) {
+                        // skip attribute processing for SCOPE_NULL rows
+                        continue;
+                    }
+                    $attrId = $attribute->getId();
+                    $backModel = $attribute->getBackendModel();
+                    $attrTable = $attribute->getBackend()->getTable();
+                    $storeIds = [0];
+
+                    if ('datetime' == $attribute->getBackendType() && strtotime($attrValue)) {
+                        $attrValue = $this->dateTime->gmDate(
+                            'Y-m-d H:i:s',
+                            $this->_localeDate->date($attrValue)->getTimestamp()
+                        );
+                    } elseif ($backModel) {
+                        $attribute->getBackend()->beforeSave($product);
+                        $attrValue = $product->getData($attribute->getAttributeCode());
+                    }
+                    if (self::SCOPE_STORE == $rowScope) {
+                        if (self::SCOPE_WEBSITE == $attribute->getIsGlobal()) {
+                            // check website defaults already set
+                            if (!isset($attributes[$attrTable][$rowSku][$attrId][$rowStore])) {
+                                $storeIds = $this->storeResolver->getStoreIdToWebsiteStoreIds($rowStore);
+                            }
+                        } elseif (self::SCOPE_STORE == $attribute->getIsGlobal()) {
+                            $storeIds = [$rowStore];
+                        }
+                        if (!isset($this->_oldSku[$rowSku])) {
+                            $storeIds[] = 0;
+                        }
+                    }
+                    foreach ($storeIds as $storeId) {
+                        if (!isset($attributes[$attrTable][$rowSku][$attrId][$storeId])) {
+                            $attributes[$attrTable][$rowSku][$attrId][$storeId] = $attrValue;
+                        }
+                    }
+                    // restore 'backend_model' to avoid 'default' setting
+                    $attribute->setBackendModel($backModel);
+                }
+            }
+
+            foreach ($bunch as $rowNum => $rowData) {
+                if ($this->getErrorAggregator()->isRowInvalid($rowNum)) {
+                    unset($bunch[$rowNum]);
+                }
+            }
+
+            $this->saveProductEntity(
+                $entityRowsIn,
+                $entityRowsUp
+            )->_saveProductWebsites(
+                $this->websitesCache
+            )->_saveProductCategories(
+                $this->categoriesCache
+            )->_saveProductTierPrices(
+                $tierPrices
+            )->_saveMediaGallery(
+                $mediaGallery
+            )->_saveProductAttributes(
+                $attributes
+            );
+
+            $this->_eventManager->dispatch(
+                'catalog_product_import_bunch_save_after',
+                ['adapter' => $this, 'bunch' => $bunch]
+            );
+        }
+        return $this;
+    }
+
+    /**
+     * Get product entity link field
+     *
+     * @return string
+     */
+    private function getProductEntityLinkField()
+    {
+        if (!$this->productEntityLinkField) {
+            $this->productEntityLinkField = $this->getMetadataPool()
+                ->getMetadata(\Magento\Catalog\Api\Data\ProductInterface::class)
+                ->getLinkField();
+        }
+        return $this->productEntityLinkField;
+    }
+
+    /**
+     * Get product entity identifier field
+     *
+     * @return string
+     */
+    private function getProductIdentifierField()
+    {
+        if (!$this->productEntityIdentifierField) {
+            $this->productEntityIdentifierField = $this->getMetadataPool()
+                ->getMetadata(\Magento\Catalog\Api\Data\ProductInterface::class)
+                ->getIdentifierField();
+        }
+        return $this->productEntityIdentifierField;
+    }
+}
