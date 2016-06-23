50e41cbec082c035b599160849fa5f6bfefee240
Paul Hachmang <paul@h-o.nl>
2016-06-23 17:21:12 +0200

    PROMO-39: Added feature to support external_ids on categories

---
+++ RowModifier/ExternalCategoryManagement.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Copyright (c) 2016 H&O E-commerce specialisten B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ho\Import\RowModifier;
+
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+
+class ExternalCategoryManagement extends AbstractRowModifier
+{
+
+    /**
+     * Mapping from category to internal mapping.
+     * @var null
+     */
+    protected $categoryMapping = null;
+
+    /**
+     * Category Collection Factory
+     * @var CategoryCollectionFactory
+     */
+    private $categoryCollectionFactory;
+
+
+    /**
+     * ExternalCategoryManagement constructor.
+     *
+     * @param CategoryCollectionFactory $categoryCollectionFactory
+     */
+    public function __construct(
+        CategoryCollectionFactory $categoryCollectionFactory
+    ) {
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
+
+    }
+
+
+    /**
+     * Add additional categories to products
+     * @return void
+     */
+    public function process()
+    {
+        $this->initCategoryMapping();
+        foreach ($this->items as &$item) {
+            $categories = explode(',', $item['categories']);
+            foreach ($categories as $category) {
+                if (isset($this->categoryMapping[$category])) {
+                    $categories = array_merge($categories, $this->categoryMapping[$category]);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Load Magento's categories with the import_group
+     * @return \[]
+     */
+    protected function initCategoryMapping()
+    {
+        $categoryCollection = $this->categoryCollectionFactory->create();
+        $categoryCollection->addNameToResult();
+        $categoryCollection->addAttributeToSelect('external_id');
+
+        foreach ($categoryCollection as $category) {
+            /** @var Category $category */
+
+            if (! $category->getData('external_id')) {
+                continue;
+            }
+
+            $structure = explode('/', $category->getPath());
+            $pathSize = count($structure);
+            if ($pathSize > 2) {
+                $path = [];
+                for ($i = 1; $i < $pathSize; $i++) {
+                    $item = $categoryCollection->getItemById($structure[$i]);
+                    if ($item instanceof Category) {
+                        $path[] = $item->getName();
+                    }
+                }
+
+                $importGroups = explode(',', $category->getData('external_id'));
+                foreach ($importGroups as $importGroup) {
+
+                    if (! isset($this->categoryMapping[trim($importGroup)])) {
+                        $this->categoryMapping[trim($importGroup)] = [];
+                    }
+                    // additional options for category referencing: name starting from base category, or category id
+                    $this->categoryMapping[trim($importGroup)][] = implode('/', $path);
+                }
+            }
+        }
+
+        if (! $this->categoryMapping) {
+            return [];
+        }
+
+        return $this->categoryMapping;
+    }
+}
---
+++ Setup/InstallData.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) 2016 H&O E-commerce specialisten B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ho\Import\Setup;
+
+use Magento\Catalog\Api\Data\CategoryAttributeInterface;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+
+    /**
+     * Init
+     *
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(CategorySetupFactory $categorySetupFactory)
+    {
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var \Magento\Catalog\Setup\CategorySetup $categorySetup */
+        $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
+
+        $categorySetup->removeAttribute(CategoryAttributeInterface::ENTITY_TYPE_CODE, 'external_id');
+        $categorySetup->addAttribute(CategoryAttributeInterface::ENTITY_TYPE_CODE, 'external_id', [
+            'type' => 'varchar',
+            'label' => 'External ID',
+            'input' => 'text',
+            'required' => false,
+            'sort_order' => 9,
+            'global' => ScopedAttributeInterface::SCOPE_GLOBAL
+        ]);
+    }
+}
