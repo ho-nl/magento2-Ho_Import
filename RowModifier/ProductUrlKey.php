3d9e403abaf5ceb12d13dc69aabd9633935c9666
Paul Hachmang <paul@h-o.nl>
2016-07-28 13:24:09 +0200

    PROMO-41: Implemented reset method to clear memory, implemented getProductUrlSuffix from config

--- RowModifier/ProductUrlKey.php
+++ RowModifier/ProductUrlKey.php
@@ -5,6 +5,8 @@
  */
 namespace Ho\Import\RowModifier;
 
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\UrlRewrite\Model\UrlFinderInterface;
 use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -19,52 +21,83 @@ use Symfony\Component\Console\Output\ConsoleOutput;
  */
 class ProductUrlKey extends AbstractRowModifier
 {
-
     /**
+     * URL Key Formatter
+     *
      * @var \Magento\Catalog\Model\Product\Url
      */
     private $urlKeyFormatter;
 
     /**
+     * URL Finder
+     *
      * @var UrlFinderInterface
      */
     private $urlFinderInterface;
 
     /**
-     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     * Product Collection
+     *
+     * @var CollectionFactory
      */
     private $productCollectionFactory;
 
     /**
+     * Id to SKU matching
+     *
      * @var string[]
      */
     private $idToSku = [];
 
+    /**
+     * Matched URL's
+     *
+     * @var string[]
+     */
     private $urls = [];
+
+    /**
+     * Product URL Suffix per store view
+     *
+     * @var string[]
+     */
+    private $productUrlSuffix = [];
+
+    /**
+     * Scope Config
+     *
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
 
     /**
      * ProductUrlKey constructor.
      *
-     * @param ConsoleOutput                                                  $consoleOutput
-     * @param \Magento\Catalog\Model\Product\Url                             $urlKeyFormatter
-     * @param UrlFinderInterface                                             $urlFinderInterface
-     *
-     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
-     *
-     * @internal param UrlRewriteFactory $urlRewriteFactory
+     * @param ConsoleOutput                      $consoleOutput
+     * @param \Magento\Catalog\Model\Product\Url $urlKeyFormatter
+     * @param UrlFinderInterface                 $urlFinderInterface
+     * @param ScopeConfigInterface               $scopeConfig
+     * @param CollectionFactory                  $productCollectionFactory
      */
     public function __construct(
         ConsoleOutput $consoleOutput,
         \Magento\Catalog\Model\Product\Url $urlKeyFormatter,
         UrlFinderInterface $urlFinderInterface,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+        ScopeConfigInterface $scopeConfig,
+        CollectionFactory $productCollectionFactory
     ) {
         parent::__construct($consoleOutput);
-        $this->urlKeyFormatter = $urlKeyFormatter;
-        $this->urlFinderInterface = $urlFinderInterface;
+        $this->urlKeyFormatter          = $urlKeyFormatter;
+        $this->urlFinderInterface       = $urlFinderInterface;
         $this->productCollectionFactory = $productCollectionFactory;
+        $this->scopeConfig              = $scopeConfig;
     }
 
+    /**
+     * Get the proper URL keys
+     *
+     * @return void
+     */
     public function process()
     {
         $this->initProductToSku();
@@ -72,64 +105,65 @@ class ProductUrlKey extends AbstractRowModifier
             //check for existence.
             $item['url_key'] = $this->getUrlKey($item['url_key'], $identifier);
         }
+        $this->reset();
     }
 
-    
     /**
      * Get the product URL-key and check for availability
      *
-     * @param $line
-     * @param $sku
-     * @param $string
+     * @param string $urlKey
+     * @param string $identifier
+     *
      * @return mixed
      */
     public function getUrlKey($urlKey, $identifier)
     {
         $options = [
             $this->urlKeyFormatter->formatUrlKey($urlKey),
-            $this->urlKeyFormatter->formatUrlKey($identifier.'-'.$urlKey),
-            $this->urlKeyFormatter->formatUrlKey($identifier.'-'.$urlKey.'-1'),
-            $this->urlKeyFormatter->formatUrlKey($identifier.'-'.$urlKey.'-2'),
-            $this->urlKeyFormatter->formatUrlKey($identifier.'-'.$urlKey.'-3'),
-            $this->urlKeyFormatter->formatUrlKey($identifier.'-'.$urlKey.'-4'),
+            $this->urlKeyFormatter->formatUrlKey($identifier . '-' . $urlKey),
+            $this->urlKeyFormatter->formatUrlKey($identifier . '-' . $urlKey . '-1'),
+            $this->urlKeyFormatter->formatUrlKey($identifier . '-' . $urlKey . '-2'),
+            $this->urlKeyFormatter->formatUrlKey($identifier . '-' . $urlKey . '-3'),
+            $this->urlKeyFormatter->formatUrlKey($identifier . '-' . $urlKey . '-4'),
         ];
         $options = array_unique($options);
-
         foreach ($options as $option) {
             if (isset($this->urls[$option])) {
                 continue;
             }
 
-            $foundKeys = $this->urlFinderInterface->findAllByData(['request_path' => $option.'.html']);
-
+            $foundKeys = $this->urlFinderInterface->findAllByData([
+                'request_path' => $option . $this->getProductUrlSuffix(0)
+            ]);
             //The URL Key doesn't exist
             if (count($foundKeys) <= 0) {
                 $this->urls[$option] = true;
                 return $option;
             }
-
             $isKeyCurrent = array_reduce($foundKeys, function ($carry, UrlRewrite $item) {
                 if ($carry) {
                     return $carry;
                 }
-
                 if (isset($this->idToSku[$item->getEntityId()])) {
                     return true;
                 }
                 return false;
             });
-
             if ($isKeyCurrent) {
                 $this->urls[$option] = true;
                 return $option;
             }
-
             continue;
         }
-
-        $this->consoleOutput->writeln("<error>Can not find available URL-key for {$identifier}, you might run into trouble</error>");
+        $this->consoleOutput->writeln(
+            "<error>Can not find available URL-key for {$identifier}, you might run into trouble</error>"
+        );
     }
 
+    /**
+     * Init mapping product_id to SKU
+     * @return void
+     */
     protected function initProductToSku()
     {
         $productCollection = $this->productCollectionFactory->create();
@@ -137,11 +171,11 @@ class ProductUrlKey extends AbstractRowModifier
         $this->idToSku = $productCollection->getConnection()->fetchPairs($select);
     }
 
-    
     /**
      * Retrieve product rewrite suffix for store
      *
      * @param int $storeId
+     *
      * @return string
      */
     protected function getProductUrlSuffix($storeId = null)
@@ -154,5 +188,16 @@ class ProductUrlKey extends AbstractRowModifier
             );
         }
         return $this->productUrlSuffix[$storeId];
+    }
+
+    /**
+     * Reset the object to clear memory
+     *
+     * @return void
+     */
+    protected function reset()
+    {
+        $this->idToSku = [];
+        $this->urls    = [];
     }
 }
