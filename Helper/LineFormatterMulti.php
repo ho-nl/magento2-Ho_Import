937ef0b2b2fde43e4aaeaf34e16a59a34c743c8a
Paul Hachmang <paul@h-o.nl>
2016-08-04 17:43:12 +0200

    PROMO-144: Implemented LineFormatterSingle and LineFormatterMulti

---
+++ Helper/LineFormatterMulti.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) 2016 H&O E-commerce specialisten B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ */
+namespace Ho\Import\Helper;
+
+
+
+class LineFormatterMulti
+{
+    const LINE_DELIMITER = \Magento\CatalogImportExport\Model\Import\Product::PSEUDO_MULTI_LINE_SEPARATOR;
+
+    /**
+     * @var LineFormatterSingle
+     */
+    private $lineFormatterSingle;
+
+    /**
+     * LineEncoderMulti constructor.
+     *
+     * @param LineFormatterSingle $lineEncoderSingle
+     */
+    public function __construct(
+        LineFormatterSingle $lineEncoderSingle
+    ) {
+        $this->lineFormatterSingle = $lineEncoderSingle;
+    }
+
+    /**
+     * Encode lines
+     *
+     * @param string[string[]] $lines
+     * @return string
+     */
+    public function encode($lines)
+    {
+        $encodedLines = array_map(function ($line) {
+            $encodedLine = $this->lineFormatterSingle->encode($line);
+
+            //@todo replace pipe characters
+
+            return $encodedLine;
+        }, $lines);
+        return implode(self::LINE_DELIMITER, $encodedLines);
+    }
+
+    /**
+     * Decode Lines
+     *
+     * @param string $line
+     * @return string[string[]]
+     */
+    public function decode($line)
+    {
+        return array_map(function ($line) {
+            return $this->lineFormatterSingle->decode($line);
+        }, explode(self::LINE_DELIMITER, $line));
+    }
+}
---
+++ Helper/LineFormatterSingle.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) 2016 H&O E-commerce specialisten B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ */
+namespace Ho\Import\Helper;
+
+class LineFormatterSingle
+{
+    const ITEM_DELIMITER = \Magento\ImportExport\Model\Import::DEFAULT_GLOBAL_MULTI_VALUE_SEPARATOR;
+    const VALUE_DELIMITER = '=';
+
+    /**
+     * Encode a line to be compatible with the importer
+     * @param string[] $line
+     * @return string
+     */
+    public function encode($line)
+    {
+        $values = [];
+        foreach ($line as $key => $value) {
+            //@todo replace equal sign and comma.
+
+            $values[] = implode(self::VALUE_DELIMITER, [$key, $value]);
+        }
+        return implode(self::ITEM_DELIMITER, $values);
+    }
+
+    /**
+     * Decode Line for a compatible importer line
+     * @param string $line
+     * @return string[]
+     */
+    public function decode($line)
+    {
+        $lineItems = explode(self::ITEM_DELIMITER, $line);
+        $values = [];
+        foreach ($lineItems as $lineItem) {
+            list($key, $value) = explode(self::VALUE_DELIMITER, $lineItem);
+            $values[$key] = $value;
+        }
+
+        return $values;
+    }
+}
