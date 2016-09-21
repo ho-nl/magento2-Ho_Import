cd031f3b390d9f7cf0a09059605cfa5291e35785
Paul Hachmang <paul@h-o.nl>
2016-09-21 22:32:26 +0200

    PROMO-267: Solve issue with indexer when option is required

--- Rewrite/ImportExport/Import/ProductOption.php
+++ Rewrite/ImportExport/Import/ProductOption.php
@@ -24,4 +24,40 @@ class ProductOption extends \Magento\CatalogImportExport\Model\Import\Product\Op
         'multiple' => true,
         'file' => ['price', 'sku', 'file_extension', 'image_size_x', 'image_size_y'],
     ];
+
+
+    /**
+     * Retrieve specific type data
+     *
+     * @param array $rowData
+     * @param int $optionTypeId
+     * @param bool $defaultStore
+     * @return array|false
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    protected function _getSpecificTypeData(array $rowData, $optionTypeId, $defaultStore = true)
+    {
+        if (!empty($rowData[self::COLUMN_ROW_TITLE]) && $defaultStore && empty($rowData[self::COLUMN_STORE])) {
+            $valueData = [
+                'option_type_id' => $optionTypeId,
+                'sort_order' => empty($rowData[self::COLUMN_ROW_SORT]) ? 0 : abs($rowData[self::COLUMN_ROW_SORT]),
+                'sku' => !empty($rowData[self::COLUMN_ROW_SKU]) ? $rowData[self::COLUMN_ROW_SKU] : '',
+            ];
+
+            $priceData = false;
+            if (isset($rowData[self::COLUMN_ROW_PRICE])) {
+                $priceData = [
+                    'price' => (double)rtrim($rowData[self::COLUMN_ROW_PRICE], '%'),
+                    'price_type' => 'fixed',
+                ];
+                if ('%' == substr($rowData[self::COLUMN_ROW_PRICE], -1)) {
+                    $priceData['price_type'] = 'percent';
+                }
+            }
+            return ['value' => $valueData, 'title' => $rowData[self::COLUMN_ROW_TITLE], 'price' => $priceData];
+        } elseif (!empty($rowData[self::COLUMN_ROW_TITLE]) && !$defaultStore && !empty($rowData[self::COLUMN_STORE])) {
+            return ['title' => $rowData[self::COLUMN_ROW_TITLE]];
+        }
+        return false;
+    }
 }
