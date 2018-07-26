8e366fba81335a1453832bf3b84237b9241ef48f
Noorul Bhoelai <noorul@h-o.nl>
2018-07-26 15:03:17 +0200

    WD2-182 - show error message while importing products

--- src/Rewrite/ErrorProcessingAggregatorLogging.php
+++ src/Rewrite/ErrorProcessingAggregatorLogging.php
@@ -46,6 +46,7 @@ class ErrorProcessingAggregatorLogging
                 $this->logger->info('export/import error, ErrorMessage:', [$error->getErrorMessage()]);
                 /** @todo temporary output rownumber, remove var_dump when logging is finished */
                 var_dump($error->getRowNumber());
+                var_dump($error->getErrorMessage());
                 $result[$key][] = $error->getRowNumber() + 1;
             }
         }
